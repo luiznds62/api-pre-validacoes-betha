6708bd52f920cea3494e4a8e16b317a01e59e71e
luiznds62 <42879594+luiznds62@users.noreply.github.com>
2019-07-10 14:12:21 -0300

    init

--- src/Server.ts
+++ src/Server.ts
@@ -1,31 +1,52 @@
-import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
+import {
+  GlobalAcceptMimesMiddleware,
+  ServerLoader,
+  ServerSettings
+} from "@tsed/common";
 import * as cors from "cors";
 import "@tsed/swagger";
-import {$log} from "ts-log-debug";
+import { $log } from "ts-log-debug";
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const compress = require("compression");
 const methodOverride = require("method-override");
 const rootDir = __dirname;
-const options:cors.CorsOptions = {
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token"
+  ],
   credentials: true,
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
   origin: "*",
   preflightContinue: false
-}
+};
 
 @ServerSettings({
   rootDir,
   acceptMimes: ["application/json"],
   mount: {
-    '/api': `${rootDir}/**/*Ctrl.ts`
+    "/api": `${rootDir}/**/*Ctrl.ts`
   },
+  componentsScan: [
+    `${rootDir}/services/**/**.ts`
+  ],
   logger: {
     debug: false,
     logRequest: true,
-    requestFields: ["reqId", "method", "url", "headers", "query", "params", "duration"]
+    requestFields: [
+      "reqId",
+      "method",
+      "url",
+      "headers",
+      "query",
+      "params",
+      "duration"
+    ]
   },
   swagger: {
     path: "/api-docs"
@@ -41,16 +62,17 @@ export class Server extends ServerLoader {
    * @returns {Server}
    */
   $onMountingMiddlewares(): void | Promise<any> {
-    this
-      .use(GlobalAcceptMimesMiddleware)
+    this.use(GlobalAcceptMimesMiddleware)
       .use(cors(options))
       .use(cookieParser())
       .use(compress({}))
       .use(methodOverride())
       .use(bodyParser.json())
-      .use(bodyParser.urlencoded({
-        extended: true
-      }));
+      .use(
+        bodyParser.urlencoded({
+          extended: true
+        })
+      );
 
     return null;
   }
